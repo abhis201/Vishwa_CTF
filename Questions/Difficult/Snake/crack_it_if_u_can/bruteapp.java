83a469b525f3906e7f35fcf5afacebe7111f9683
abhis201 <31624329+abhis201@users.noreply.github.com>
2019-03-18 10:49:59 +0530

    Add files via upload

---
+++ Questions/Difficult/Snake/crack_it_if_u_can/bruteapp.java
@@ -0,0 +1,33 @@
+    import java.util.Scanner;
+
+    public class User_Authentication 
+
+    {
+
+        public static void main(String args[])
+
+        {
+
+            System.out.print("Enter password:");//password:user
+
+            password = s.nextLine();
+
+            if(password.equals("vishwaCTF{hashcat}"))
+
+            {
+
+                System.out.println("Authentication Successful");
+
+            }
+
+            else
+
+            {
+
+                System.out.println("Authentication Failed");
+
+            }
+
+        }
+
+    }
